f57dfd51a29fdfe99db8aef2b7182992fae8d350
Utkarshende <139004110+Utkarshende@users.noreply.github.com>
2025-08-21 16:03:11 +0530

    new folders ans files created

---
+++ src/assets/views/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+import Navbar from '../components/Navbar'
+import FeatureSection from '../components/FeatureSection'
+
+
+function Home() {
+  return (
+    <div>
+    <Navbar />
+    <HeroSection />
+    <FeatureSection />
+    </div>
+  )
+}
+
+export default Home
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Home from './assets/views/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+    <Home />
+
 )
