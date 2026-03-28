717fa73e650a92968a6ea3113baac33856afbe2b
Utkarshende <139004110+Utkarshende@users.noreply.github.com>
2026-03-28 17:49:41 +0530

    Removed Back to Home Link from AllMaid component

--- src/assets/views/AllMaid.jsx
+++ src/assets/views/AllMaid.jsx
@@ -108,7 +108,6 @@ function AllMaid() {
                     ))}
                 </div>
                 
-                {/* Back to Home Link */}
                 <div className='mt-12 text-center'>
                     <Link to="/" className='text-teal-600 hover:text-teal-800 font-medium flex items-center justify-center transition'>
                         <HomeIcon className='w-5 h-5 mr-2'/> Return to Homepage
